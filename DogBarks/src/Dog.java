f2a058b97e29761a6a52afb0cff6d3e43deea5ef
MD HAQUE <MdEmadulHaque@MDs-MBP-3.home>
2017-05-08 20:08:14 -0400

    emadulhaque

---
+++ DogBarks/src/Dog.java
@@ -0,0 +1,21 @@
+
+public class Dog 
+{
+	int size;
+	String name;
+	public void bark()
+	{
+		if (size>60)
+		{
+			System.out.println("Woof Woof");
+		}
+		else if(size>=14)
+		{
+			System.out.println("Dog barks Hop Hop");
+		}
+		else
+		{
+			System.out.println("dog desn't bark");
+		}
+	}
+}
